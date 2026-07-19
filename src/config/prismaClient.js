9841dac928edbf937dc2451987f04f20a97355b8
Marta Bosquet <martabosquetlauci@gmail.com>
2026-07-19 11:50:10 +0200

    mejoras front-end

--- src/config/prismaClient.js
+++ src/config/prismaClient.js
@@ -2,7 +2,7 @@ import 'dotenv/config'
 import { PrismaClient } from "@prisma/client"
 import { PrismaPg } from "@prisma/adapter-pg" // adaptador de prisma para postgreSQL
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL || process.env.DIRECT_URL })
+const adapter = new PrismaPg({ connectionString: process.env.DIRECT_URL || process.env.DATABASE_URL })
 const prisma = new PrismaClient({ adapter })
 
 export default prisma
