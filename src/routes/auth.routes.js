f76816d137944ea75f81b30b06063bea0bb51cdc
Marta Bosquet <martabosquetlauci@gmail.com>
2026-08-11 16:07:59 +0200

    arreglos fluidez

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -12,15 +12,15 @@ const router = express.Router()
 router.post('/api/auth/register', upload.single('profileImage'), authController.register);
 router.post("/api/auth/login", authController.login);
 router.post("/api/auth/logout", authController.logout);
+router.get("/api/users/:userId", authController.getPublicProfile);  // Obtener perfil público de otro usuario
 
 // Ruta protegida (accedemos al perfil de usuario con el token)
 router.get("/api/me", authMiddleware, authController.getProfile);
 router.put("/api/me", authMiddleware, upload.single('profileImage'), authController.updateProfile);  // AQUÍ AÑADIMOS upload.single('profileImage') para que capture el archivo y lo suba a Cloudinary
 router.put("/api/me/password", authMiddleware, authController.updatePassword); // Para cambiar contraseña
 router.delete("/api/me", authMiddleware, authController.deleteAccount); // Para eliminar cuenta
-router.patch("/api/profile",authMiddleware,upload.single("profileImage"),authController.updateProfile);  // Actualizar datos personales
-router.patch("/api/profile/cinephile",authMiddleware,authController.updateCinephileProfile);  // Actualizar perfil cinéfilo
-router.get("/api/users/:userId", authMiddleware, authController.getPublicProfile);  // Obtener perfil público de otro usuario
+router.patch("/api/profile", authMiddleware, upload.single("profileImage"), authController.updateProfile);  // Actualizar datos personales
+router.patch("/api/profile/cinephile", authMiddleware, authController.updateCinephileProfile);  // Actualizar perfil cinéfilo
 
 // Ruta restringida por rol (Panel de admin)
 router.get(
